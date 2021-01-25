2af7ca536a9f0e50efde0d731de0846098dbcb19
Shamdan17 <shamdan17@ku.edu.tr>
2021-01-25 07:20:40 +0300

    Make data structs mutable, add label setting and removing, bug fixes, add instance flattening for ReCoRD and MultiRC (instance with multiple questions, answers, to multiple instances with one question, one (or more) answers each

--- src/data.jl
+++ src/data.jl
@@ -1,5 +1,6 @@
 using JSON
 using Test
+using Random
 
 # A dictionary containing the names of all defined datatypes
 registeredtypes = Dict{String, Any}()
@@ -22,6 +23,15 @@ end
 
 function getLabel(d::datum)
     error("Not implemented for this datatype. Please define the function for this datatype.")
+end
+
+# Returns an array of datum objects with exclusively only 1 label per datum.
+function flatten(d::datum; o...)
+    [d]
+end
+
+function flatten(d::AbstractArray{<:datum}; o...)
+    d
 end
 
 
@@ -58,18 +68,32 @@ function getLabel(b::BoolQ)
     return b.label ? 2 : 1
 end
 
+function setLabel(b::BoolQ, label::Int)
+    b.label = 2==label
+    b.labeled = true
+end
+
+function setLabel(b::BoolQ, label::Bool)
+    b.label = label
+    b.labeled = true
+end
+
+function removeLabel(b::BoolQ)
+    b.labeled = false
+end
+
 # CommitmentBank data object
 # Original paper: https://semanticsarchive.net/Archive/Tg3ZGI2M/Marneffe.pdf
 mutable struct CB <: datum
     premise::String     # Premise
     hypothesis::String  # Hypothesis
     label::String       # Whether the hypothesis is an entailment, contradiction, or neutral to the passage
-    idx::Int            # Index in the original dataset
+    idx::String         # Index in the original dataset
     labeled::Bool       # Whether the data object has a label. If false, the label is meaningless
     logits              # logits for all the labels
-    CB(premise::String, hypothesis::String,label::String, idx::Int) = new(premise, hypothesis, label, idx, true, nothing)
-    CB(premise::String, hypothesis::String,label::Nothing, idx::Int) = new(premise, hypothesis, "None", idx, false, nothing)
-    CB(premise::String, hypothesis::String, idx::Int) = new(premise, hypothesis, "None", idx, false, nothing)
+    CB(premise::String, hypothesis::String,label::String, idx::String) = new(premise, hypothesis, label, idx, true, nothing)
+    CB(premise::String, hypothesis::String,label::Nothing, idx::String) = new(premise, hypothesis, "None", idx, false, nothing)
+    CB(premise::String, hypothesis::String, idx::String) = new(premise, hypothesis, "None", idx, false, nothing)
 end
 
 @registerDataType "CB" CB
@@ -79,7 +103,7 @@ function CB(json::AbstractString)
     CB(contents["premise"],
           contents["hypothesis"],
           get(contents, "label", nothing),
-          contents["idx"])
+          "$(contents["idx"])")
 end
 
 function getChoices(c::CB)
@@ -88,7 +112,22 @@ end
 
 function getLabel(c::CB)
     @assert c.labeled "This instance is not labeled."
-    return argmax(["entailment", "contradiction", "neutral"].==c.label)
+    return argmax(["contradiction", "entailment", "neutral"].==c.label)
+end
+
+
+function setLabel(cb::CB, label::Int)
+    cb.label = ["contradiction", "entailment", "neutral"][label]
+    cb.labeled = true
+end
+
+function setLabel(cb::CB, label::String)
+    cb.label = label
+    cb.labeled = true
+end
+
+function removeLabel(cb::CB)
+    cb.labeled = false
 end
 
 # COPA data object
@@ -111,16 +150,20 @@ end
 
 function COPA(json::AbstractString)
     contents = JSON.parse(json)
+    label = get(contents,"label",nothing)
+    if label != nothing
+        label+=1
+    end
     COPA(contents["premise"],
           contents["choice1"],
           contents["choice2"],
           contents["question"],
-          get(contents,"label",nothing),
+          label,
           contents["idx"])
 end
 
 function getChoices(c::COPA)
-    return [0, 1]
+    return [1, 2]
 end
 
 function getLabel(c::COPA)
@@ -128,7 +171,28 @@ function getLabel(c::COPA)
     return c.label
 end
 
+function setLabel(cp::COPA, label::Int)
+    cp.label = label
+    cp.labeled = true
+end
 
+function removeLabel(cp::COPA)
+    cp.labeled = false
+end
+
+# Adds a mirrored copy of the instance
+function flatten(d::COPA; o...)
+    mr = deepcopy(d)
+    tmp = mr.choice1
+    mr.choice1 = mr.choice2
+    mr.choice2 = tmp
+    mr.label = 3-mr.label
+    [d, mr]
+end
+
+function flatten(d::AbstractArray{<:COPA}; o...)
+    flattened = [Iterators.flatten(flatten.(d))...]
+end
 
 # MultiRC data object
 # Original paper: https://www.aclweb.org/anthology/N18-1023/
@@ -165,8 +229,10 @@ end
 mutable struct MultiRC <: datum
     passage::String     # The passage
     questions::Array{MultiRCQuestion} # An array of questions about the passage along with answer options
-    idx::Int            # Index in the original dataset
+    idx                 # Index in the original dataset
     labeled::Bool       # Whether the questions have their correct answers labeled
+    logits              # logits for all the labels
+    MultiRC(passage::String, questions::Array{MultiRCQuestion}, idx, labeled::Bool)=new(passage, questions, idx, labeled, nothing)
 end
 
 function MultiRC(json::AbstractString)
@@ -178,16 +244,56 @@ function MultiRC(json::AbstractString)
     MultiRC(passage, questions, idx, labeled)
 end
 
+function flatten(d::MultiRC; o...)
+    flattened = []
+    for q in d.questions
+        for a in q.options
+            curq = MultiRCQuestion(q.question, [a], q.idx, a.labeled)
+            curmrc = MultiRC(d.passage, [curq], [d.idx, q.idx, a.idx], d.labeled)
+            push!(flattened, curmrc)
+        end
+    end
+    flattened
+end
+
+function flatten(d::AbstractArray{MultiRC}; o...)
+    flattened = [Iterators.flatten(flatten.(d))...]
+end
+
 @registerDataType "MultiRC" MultiRC
 
 
 function getChoices(m::MultiRC)
-    return [[a.text for a in q.options] for q in m.questions]
+    if length(m.questions)==1&&length(m.questions[1].options)==1
+        return m.questions[1].options[1].text
+    else
+        return [[a.text for a in q.options] for q in m.questions]
+    end
 end
 
 function getLabel(m::MultiRC)
     @assert m.labeled "This instance is not labeled."
-    return [[a.label for a in q.options] for q in m.questions]
+    if length(m.questions)==1&&length(m.questions[1].options)==1
+        return m.questions[1].options[1].label ? 2 : 1
+    else
+        return [[a.label ? 2 : 1 for a in q.options] for q in m.questions]
+    end
+end
+
+
+function setLabel(m::MultiRC, label::Int)
+    @assert length(m.questions)==1 && length(m.questions[1].options)==1 "Only flattened MultiRC instances support setLabel"
+    m.questions[1].options[1].label = label == 2
+    m.labeled = true
+    m.questions[1].labeled=true
+    m.questions[1].options[1].labeled=true
+end
+
+function removeLabel(m::MultiRC)
+    @assert length(m.questions)==1 && length(m.questions[1].options)==1 "Only flattened MultiRC instances support removeLabel"
+    m.labeled = false
+    m.questions[1].labeled=false
+    m.questions[1].options[1].labeled=false
 end
 
 
@@ -243,6 +349,8 @@ mutable struct ReCoRD <: datum
     qas::Array{ReCoRDQuestion} # Array of questions
     idx::Int            # Index in the original dataset
     labeled::Bool       # Whether the questions have their correct answers labeled
+    logits              # logits for all the labels
+    ReCoRD(source::String,text::String,entities::Array,qas::Array,idx::Int,labeled::Bool)=new(source,text,entities,qas,idx,labeled,nothing)
 end
 
 @registerDataType "ReCoRD" ReCoRD
@@ -251,6 +359,7 @@ function ReCoRD(json::AbstractString)
     content = JSON.parse(json)
     source = content["source"]
     text = content["passage"]["text"]
+    # text = replace(content["passage"]["text"], "@highlight\n"=>"- ") -> Do this at the end, otherwise the indices are messed up
     entities = [ReCoRDEntity(q, text) for q in content["passage"]["entities"]]
     questions = [ReCoRDQuestion(q, text) for q in content["qas"]]
     labeled = questions[1].labeled
@@ -265,26 +374,73 @@ function getChoices(r::ReCoRD)
     return [ents for q in r.qas]
 end
 
+# function getLabel(r::ReCoRD)
+#     @assert r.labeled "This instance is not labeled."
+#     ents = getChoices(r)
+#     answers = [[(ent in [a.text for a in q.answers]) ? 1 : 0 for ent in ents[1]] for q in r.qas]
+#     return answers
+# end
+
 function getLabel(r::ReCoRD)
     @assert r.labeled "This instance is not labeled."
-    ents = getChoices(r)
-    answers = [[(ent in [a.text for a in q.answers]) ? 1 : 0 for ent in ents[1]] for q in r.qas]
-    return answers
+    return 2
 end
 
 
+function flatten(d::ReCoRD; rng=MersenneTwister(42), max_train_candidates_per_question=10, set="train")
+    flattened = []
+    for q in d.qas
+        answers = [a.text for a in q.answers]
+        candidates = [e for e in getChoices(d)[1] if !(e in answers)]
+        # Labeled
+        if set=="train"
+            for (idx, a) in enumerate(Set(answers))
+                cur_candidates = candidates[randperm(rng, length(candidates))]
+                if length(candidates)>max_train_candidates_per_question-1
+                    cur_candidates = cur_candidates[1:(max_train_candidates_per_question-1)]
+                end
+                cur_entities = [ReCoRDEntity(-1, -1, x) for x in [a, cur_candidates...]]
+                cur_answer = ReCoRDAnswer(-1, -1, a)
+                cur_question = deepcopy(q)
+                cur_question.answers = [cur_answer]
+
+                cur_datum = deepcopy(d)
+                cur_datum.entities = cur_entities
+                cur_datum.qas = [cur_question]
+
+                push!(flattened, cur_datum)
+            end
+        else #Unlabeled
+            cur_question = deepcopy(q)
+
+            cur_datum = deepcopy(d)
+            cur_datum.qas = [cur_question]
+
+            uniqueents = [ReCoRDEntity(-1, -1, e) for e in getChoices(d)[1]]
+            cur_datum.entities = uniqueents
+
+            push!(flattened, cur_datum)
+        end
+    end
+    flattened
+end
+
+function flatten(d::AbstractArray{ReCoRD}; set="train")
+    rng = MersenneTwister(42);
+    flattened = [Iterators.flatten(flatten.(d, rng=rng, set=set))...]
+end
 
 # RTE data object
 mutable struct RTE <: datum
     premise::String     # Premise
     hypothesis::String  # Hypothesis
     label::String       # Whether the hypothesis is an entailment, or not an entailment to the passage
-    idx::Int            # Index in the original dataset
+    idx::String         # Index in the original dataset
     labeled::Bool       # Whether the questions have their correct answers labeled
     logits              # logits for all the labels
-    RTE(premise::String, hypothesis::String, label::String, idx::Int64) = new(premise, hypothesis, label, idx, true, nothing)
-    RTE(premise::String, hypothesis::String, label::Nothing, idx::Int64) = new(premise, hypothesis, "None", idx, false, nothing)
-    RTE(premise::String, hypothesis::String, idx::Int64) = new(premise, hypothesis, "None", idx, false, nothing)
+    RTE(premise::String, hypothesis::String, label::String, idx::String) = new(premise, hypothesis, label, idx, true, nothing)
+    RTE(premise::String, hypothesis::String, label::Nothing, idx::String) = new(premise, hypothesis, "None", idx, false, nothing)
+    RTE(premise::String, hypothesis::String, idx::String) = new(premise, hypothesis, "None", idx, false, nothing)
 end
 
 @registerDataType "RTE" RTE
@@ -294,7 +450,7 @@ function RTE(json::AbstractString)
     RTE(contents["premise"],
           contents["hypothesis"],
           get(contents,"label",nothing),
-          contents["idx"])
+          "$(contents["idx"])")
 end
 
 function getChoices(r::RTE)
@@ -304,6 +460,20 @@ end
 function getLabel(r::RTE)
     @assert r.labeled "This instance is not labeled."
     return argmax(["entailment", "not_entailment"].==r.label)
+end
+
+function setLabel(r::RTE, label::Int)
+    r.label = ["entailment", "not_entailment"][label]
+    r.labeled = true
+end
+
+function setLabel(r::RTE, label::String)
+    r.label = label
+    r.labeled = true
+end
+
+function removeLabel(r::RTE)
+    r.labeled = false
 end
 
 
@@ -350,6 +520,19 @@ function getLabel(w::WiC)
     return w.label ? 2 : 1
 end
 
+function setLabel(w::WiC, label::Int)
+    w.label = label==2
+    w.labeled = true
+end
+
+function setLabel(w::WiC, label::Bool)
+    w.label = label
+    w.labeled = true
+end
+
+function removeLabel(w::WiC)
+    w.labeled = false
+end
 
 # WSC data object
 mutable struct WSC <: datum
@@ -373,9 +556,9 @@ function WSC(json::AbstractString)
     contents = JSON.parse(json)
     WSC(contents["text"],
         get(contents,"label",nothing),
-        contents["target"]["span1_index"],
+        contents["target"]["span1_index"]+1,
         contents["target"]["span1_text"],
-        contents["target"]["span2_index"],
+        contents["target"]["span2_index"]+1,
         contents["target"]["span2_text"],
         contents["idx"])
 end
@@ -390,6 +573,20 @@ function getLabel(w::WSC)
     return w.label ? 2 : 1
 end
 
+
+function setLabel(w::WSC, label::Int)
+    w.label = label==2
+    w.labeled = true
+end
+
+function setLabel(w::WSC, label::Bool)
+    w.label = label
+    w.labeled = true
+end
+
+function removeLabel(w::WSC)
+    w.labeled = false
+end
 
 @testset "Testing datatype constructors" begin
     for (name, datatype) in registeredtypes
